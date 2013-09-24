6b5eab6648e497147b3195c8a26f050348b5fde9
Steen Larsen <steen666@gmail.com>
2013-09-24 16:46:58 +0200

    return boolean

--- src/orderrules/core.clj
+++ src/orderrules/core.clj
@@ -17,24 +17,36 @@
 (fact canstart :dtv :aktiv)
 (fact canstart :dtv :kunsignal)
 
+(defrel hwstatusok fo status)
+(fact hwstatusok :clear nil)
+(fact hwstatusok :bb :sent)
+(fact hwstatusok :bb :received)
+(fact hwstatusok :tlf :sent)
+(fact hwstatusok :tlf :received)
+
+(defrel teknikerok fo status)
+(fact teknikerok :clear :done)
+(fact teknikerok :bb :done)
+(fact teknikerok :tlf :done)
+
 (defrel hdok val)
 (fact hdok true)
 
-(defn handlingsdato<today? [hd]
+(defn handlingsdato-ok? [hd]
   true)
 
-(defn cancreate [o al]
-  (run* [q]
-        (fresh [needed status]
-               (conde
-                [(membero {:a needed :s status} al)]
-                [(== needed nil)])
-               (needs (:a o) needed)
-               (hdok (handlingsdato<today? (:hd o)))
-               (canstart (:a o) status))))
+(defn create-subscription? [o al]
+  (= '(_0) (run* [q]
+         (fresh [needed status]
+                (conde
+                 [(membero {:a needed :s status} al)]
+                 [(== needed nil)])
+                (needs (:a o) needed)
+                (hdok (handlingsdato-ok? (:hd o)))
+                (canstart (:a o) status)))))
 
-(def o {:a :youbio :hd "11-09-2013"})
+(def o {:a :bb :hd "11-09-2013"})
 
 (def l [{:s :aktiv :a :clear}])
 
-(cancreate o l)
+(create-subscription? o l)
